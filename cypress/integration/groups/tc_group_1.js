e47ae8c89d9e0666068814496782f425a10367b2
github_shubhamK010 <shubhamkmb@gmail.com>
2022-07-20 16:38:55 +0530

    cypressGrep

---
+++ cypress/integration/groups/tc_group_1.js
@@ -0,0 +1,14 @@
+describe('group testcase one',function(){
+
+    it('smoke test',function(){
+        expect(1).to.eql(1)
+    })
+
+    it('regression test',function(){
+        expect(1).to.eql(1)
+    })
+
+    it('sprint test',function(){
+        expect(1).to.eql(1)
+    })
+})
---
+++ cypress/integration/groups/tc_group_3.js
@@ -0,0 +1,28 @@
+describe('verify the test cases by tags',{tags:['@space']}, function () {
+
+    it('smoke test', { tags: ['@smoke'] }, function () {
+        expect(1).to.eql(1)
+    })
+    it('regression test', { tags: ['@regression'] }, function () {
+        expect(1).to.eql(1)
+    })
+    it('sprint test', { tags: ['@sprint'] }, function () {
+        expect(1).to.eql(1)
+    })
+
+    it('smoke test and regression test', { tags: ['@smoke', '@regression'] }, function () {
+        expect(1).to.eql(1)
+    })
+    it('smoke test and regression test', { tags: ['@smoke', '@regression'] }, function () {
+        expect(1).to.eql(1)
+    })
+    it('smoke test and regression test', { tags: ['@smoke', '@regression'] }, function () {
+        expect(1).to.eql(1)
+    })
+    it('sprint test and regression test', { tags: ['@sprint', '@regression'] }, function () {
+        expect(1).to.eql(1)
+    })
+    it('smoke and sprint test', { tags: ['@smoke', '@sprint'] }, function () {
+        expect(1).to.eql(1)
+    })
+})
